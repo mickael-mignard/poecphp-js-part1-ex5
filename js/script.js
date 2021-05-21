eb469526b2b1444c47a60173fe925204524fb967
Mickael Mignard <mignard.mickael@gmail.com>
2021-05-21 09:14:28 +0200

    Reprise du code et ajout d'une autre manière de vérifier si un input correspond à un float

--- js/script.js
+++ js/script.js
@@ -1,12 +1,77 @@
+/**
+ * Compute result
+ * with isNan and indexOf to check if inputs are decimal numbers
+  */
+function computeResult() {
+    // Get inputs
+    let number1 = document.getElementById("number1").value;
+    let number2 = document.getElementById("number2").value;
 
-function afficher(){
-    var nombre1 = document.getElementById("nombre1").value;
-    var nombre2 = document.getElementById("nombre2").value;
-    
-    nombre1 = Math.trunc(nombre1);
-    var resultat = nombre1 * nombre2;
+    // Replace comma by point if necessary
+    number1 = number1.replace(',', '.');
+    number2 = number2.replace(',', '.');
 
-    alert(
-        "Résultat de la multiplication, après troncature du premier nombre : " + resultat
-    )
+    // Check if inputs are decimal numbers. If not, script is stopped.
+    if ((!isNaN(number1) && number1.indexOf(".") == -1)
+        || (!isNaN(number2) && number2.indexOf(".") == -1)) {
+        alert("Saisissez deux nombres décimaux");       
+        return;
     }
+
+    // Truncate first input and compute result
+    number1 = Math.trunc(number1);
+    return "Résultat = " +  number1 * number2;    
+}
+
+/**
+ * Display result
+ */
+function displayResult(){    
+    var result = computeResult();
+    if(result === undefined){
+        document.getElementById('result').innerHTML = "Calcul impossible ! Saisir deux nombres décimaux !"
+    }else{
+        document.getElementById('result').innerHTML = result;
+    }    
+}
+
+
+// /**
+//  * Compute result
+//  * with Number() and modulo to check if inputs are decimal numbers
+//  */
+// function computeResult() {
+//     // Get inputs
+//     let number1 = document.getElementById("number1").value;
+//     let number2 = document.getElementById("number2").value;
+
+//     // Replace comma by point if necessary
+//     number1 = number1.replace(',', '.');
+//     number2 = number2.replace(',', '.');
+
+//     // Check if inputs are decimal numbers. If not, script is stopped
+//     if (!isFloat(number1) || !isFloat(number2)) {
+//         alert("Saisissez deux nombres décimaux");
+//         return;
+//     }
+
+//     // Truncate first input and calculate result
+//     number1 = Math.trunc(number1);
+//     return "Résultat = " +  number1 * number2;     
+// }
+
+// /**
+//  * Check if input is a decimal number
+//  * 
+//  * @param input to check
+//  * @returns true if decimal number
+//  */
+// function isFloat(input) {
+//     return Number(input) && input % 1 !== 0;
+// }
+
+
+
+
+// console.log(Number.isNaN("azerty"));
+// console.log(isNaN("azerty"));
